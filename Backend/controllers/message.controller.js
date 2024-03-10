feec469afe9e0fd41b37c9a386542572682117e4
animesh65432 <kirandutta234@gmail.com>
2024-03-10 20:52:30 +0530

    get method

--- Backend/controllers/message.controller.js
+++ Backend/controllers/message.controller.js
@@ -35,3 +35,19 @@ export const sendmessage = async (req, res) => {
     return res.status(500).json({ error: "Internal server error" });
   }
 };
+
+export const getmessage = async (req, res) => {
+  try {
+    const { id: useridtochat } = req.params;
+    const senderid = req.user._id;
+
+    const converstaion = await Conversation.findOne({
+      participants: { $all: [senderid, useridtochat] },
+    }).populate("messages");
+
+    res.status(200).json(converstaion.messages);
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({ message: "Internal Server Errors" });
+  }
+};
--- Backend/router/message.router.js
+++ Backend/router/message.router.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { protect } from "../middlewares/protect.js";
-import { sendmessage } from "../controllers/message.controller.js";
+import { getmessage, sendmessage } from "../controllers/message.controller.js";
 
 const router = express.Router();
 
+router.get("/:id", protect, getmessage);
 router.post("/send/:id", protect, sendmessage);
 
 export default router;
