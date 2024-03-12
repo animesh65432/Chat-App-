d24fc1004f87860211af5eab06ed2e1e91f7ae04
animesh65432 <kirandutta234@gmail.com>
2024-03-12 13:13:14 +0530

    fix bugs

--- Backend/server.js
+++ Backend/server.js
@@ -16,12 +16,18 @@ const __dirname = path.resolve();
 
 dotenv.config();
 
-app.use(express.json());
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectToMongoDB();
